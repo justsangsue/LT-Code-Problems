bf4dd2e02605cd16b3b4c62b61598d7c516fcf82
justsangsue <justsangsue@gmail.com>
2019-08-20 11:48:29 -0400

    inroder successor in BST

---
+++ 41_inorder_successor_in_BST/readme.md
@@ -0,0 +1,2 @@
+1. Fully understand the question
+2. if using helper, do not forget to write exit condition
---
+++ 41_inorder_successor_in_BST/solution.py
@@ -0,0 +1,30 @@
+"""
+Definition for a binary tree node.
+class TreeNode(object):
+    def __init__(self, x):
+        self.val = x
+        self.left = None
+        self.right = None
+"""
+
+
+class Solution:
+    """
+    @param: root: The root of the BST.
+    @param: p: You need find the successor node of p.
+    @return: Successor of p.
+    """
+    def inorderSuccessor(self, root, p):
+        # write your code here
+        if p == None or root == None:
+            return None
+        return self.dfs(root, p)
+    
+    def dfs(self, root, p):
+        if root == None:
+            return root
+        if root.val <= p.val:
+            return self.dfs(root.right, p)
+        else:
+            leftAns = self.dfs(root.left, p)
+            return root if leftAns == None else leftAns
